9e63d45b5a28f98fe4de2ef89d2372134a31824f
Vitor Carniel <vitorvc00@edu.univali.br>
2018-08-21 12:04:10 -0300

    Algumas alteracoes

--- exercicio-01/Ex3Java.java
+++ exercicio-01/Ex3Java.java
@@ -1,49 +1,44 @@
-﻿
-
-
-public class Ex3Java{
-
-
-
-	public static void main(String Args[]){
+﻿public class Ex3Java{
+	public static void main(String[] args){
 		
-		if(Args[0]=='1'){
+		if(args[0]=="1"){
 					System.out.println("Janeiro.");
 				}
-		if(Args[0]=='2'){
+		if(args[0]=="2"){
 					System.out.println("Fevereiro.");
 				}
-		if(Args[0]=='3'){
+		if(args[0]=="3"){
 					System.out.println("Março.");
 				}
-		if(Args[0]=='4'){
+		if(args[0]=="4"){
 					System.out.println("Abril.");
 				}
-		if(Args[0]=='5'){
+		if(args[0]=="5"){
 					System.out.println("Maio.");
 				}
-		if(Args[0]=='6'){
+		if(args[0]=="6"){
 					System.out.println("Junho.");
 				}
-		if(Args[0]=='7'){
+		if(args[0]=="7"){
 					System.out.println("Julho.");
 				}
-		if(Args[0]=='8'){
+		if(args[0]=="8"){
 					System.out.println("Agosto.");
 				}
-		if(Args[0]=='9'){
+		if(args[0]=="9"){
 					System.out.println("Setembro.");
 				}
-		if(Args[0]=='10'){
+		if(args[0]=="10"){
 					System.out.println("Outubro.");
 				}
-		if(Args[0]=='11'){
+		if(args[0]=="11"){
 					System.out.println("Novembro.");
 				}
-		if(Args[0]=='12'){
+		if(args[0]=="12"){
 					System.out.println("Dezembro.");
 				}
 		else{
 			System.out.println("Numero invalido.");
 		}
+	}
 }
