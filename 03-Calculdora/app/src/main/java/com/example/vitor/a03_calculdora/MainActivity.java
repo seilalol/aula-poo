83e409730e24077eca7933cf1d0bc58ce7d501c1
Vitor Matheus Carniel <vitorvc00@edu.univali.br>
2018-10-09 00:08:45 -0300

    03-Calculadora - versão 0.1

--- 03-Calculdora/app/src/main/java/com/example/vitor/a03_calculdora/MainActivity.java
+++ 03-Calculdora/app/src/main/java/com/example/vitor/a03_calculdora/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static java.lang.Boolean.FALSE;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -19,17 +21,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void Go(View elementoClicaco)
     {
-        CheckBox peso, velMed;
+        CheckBox peso, velMed,MRU;
         peso = findViewById(R.id.peso);
         velMed = findViewById(R.id.velocidade);
+        MRU = findViewById(R.id.MRU);
         int naopode=0;
         if(peso.isChecked()){
             naopode+=1;
         }if(velMed.isChecked()){
             naopode+=1;
-        }
+        }if(MRU.isChecked()){
+        naopode+=1;
+    }
+        if(MRU.isChecked() && naopode<=1) {
 
-        if(velMed.isChecked() && naopode<=1) {
+            Button go = findViewById(R.id.Go);
+            Button voltar = findViewById(R.id.voltar);
+            Button calc = findViewById(R.id.calcular);
+
+            EditText xinicial, tempo,velocidade;
+
+            velocidade = findViewById(R.id.vel);
+            xinicial = findViewById(R.id.x0);
+            tempo = findViewById(R.id.tempo);
+
+            peso.setVisibility(View.INVISIBLE);
+            velMed.setVisibility(View.INVISIBLE);
+            MRU.setVisibility(View.INVISIBLE);
+            go.setVisibility(View.INVISIBLE);
+            voltar.setVisibility(View.VISIBLE);
+            calc.setVisibility(View.VISIBLE);
+            xinicial.setVisibility(View.VISIBLE);
+            tempo.setVisibility(View.VISIBLE);
+            velocidade.setVisibility(View.VISIBLE);
+
+
+        }if(velMed.isChecked() && naopode<=1) {
+
             Button go = findViewById(R.id.Go);
             Button voltar = findViewById(R.id.voltar);
             Button calc = findViewById(R.id.calcular);
@@ -41,14 +69,15 @@ public class MainActivity extends AppCompatActivity {
 
             peso.setVisibility(View.INVISIBLE);
             velMed.setVisibility(View.INVISIBLE);
+            MRU.setVisibility(View.INVISIBLE);
             go.setVisibility(View.INVISIBLE);
             voltar.setVisibility(View.VISIBLE);
             calc.setVisibility(View.VISIBLE);
             distancia.setVisibility(View.VISIBLE);
             tempo.setVisibility(View.VISIBLE);
 
-
         }if(peso.isChecked() && naopode<=1) {
+
             Button calc = findViewById(R.id.calcular);
             Button go = findViewById(R.id.Go);
             Button voltar = findViewById(R.id.voltar);
@@ -59,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
             go.setVisibility(View.INVISIBLE);
             voltar.setVisibility(View.VISIBLE);
             calc.setVisibility(View.VISIBLE);
+            MRU.setVisibility(View.INVISIBLE);
             peso.setVisibility(View.INVISIBLE);
             velMed.setVisibility(View.INVISIBLE);
 
@@ -69,44 +99,89 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void voltar(View elementoClicado){
-        CheckBox peso, velMed;
-        peso = findViewById(R.id.peso);
-        velMed = findViewById(R.id.velocidade);
-        peso.setVisibility(View.VISIBLE);
-        velMed.setVisibility(View.VISIBLE);
-        EditText distancia, tempo;
+        CheckBox peso, velMed, MRU;
 
-        Button go = findViewById(R.id.Go);
-        go.setVisibility(View.VISIBLE);
-        Button voltar = findViewById(R.id.voltar);
-        voltar.setVisibility(View.INVISIBLE);
-
+        EditText distancia, tempo,vel ,x0;
+        EditText mass = findViewById(R.id.massa);
         distancia = findViewById(R.id.distancia);
         tempo = findViewById(R.id.tempo);
-        distancia.setVisibility(View.INVISIBLE);
-        tempo.setVisibility(View.INVISIBLE);
+        MRU = findViewById(R.id.MRU);
+        x0 = findViewById(R.id.x0);
+        peso = findViewById(R.id.peso);
+        vel= findViewById(R.id.vel);
+        velMed = findViewById(R.id.velocidade);
+        Button calcular = findViewById(R.id.calcular);
 
-        EditText mass = findViewById(R.id.massa);
-        mass.setVisibility(View.INVISIBLE);
+        Button go = findViewById(R.id.Go);
+        Button voltar = findViewById(R.id.voltar);
+        TextView result = findViewById(R.id.resultado);
 
         mass.setText("");
         distancia.setText("");
         tempo.setText("");
-        Button calcular = findViewById(R.id.calcular);
-        calcular.setVisibility(View.INVISIBLE);
-
-        TextView result = findViewById(R.id.resultado);
         result.setText("");
+        vel.setText("");
+        x0.setText("");
+
+        MRU.setVisibility(View.VISIBLE);
+        x0.setVisibility(View.INVISIBLE);
+        peso.setVisibility(View.VISIBLE);
+        velMed.setVisibility(View.VISIBLE);
+        go.setVisibility(View.VISIBLE);
+        voltar.setVisibility(View.INVISIBLE);
+        distancia.setVisibility(View.INVISIBLE);
+        tempo.setVisibility(View.INVISIBLE);
+        mass.setVisibility(View.INVISIBLE);
+        vel.setVisibility(View.INVISIBLE);
+        calcular.setVisibility(View.INVISIBLE);
         result.setVisibility(View.VISIBLE);
 
     }
 
     public void calcular(View elementoClicado){
-        CheckBox peso, velMed;
+        CheckBox peso, velMed,MRU;
         peso = findViewById(R.id.peso);
         velMed = findViewById(R.id.velocidade);
+        MRU = findViewById(R.id.MRU);
 
-        if(velMed.isChecked()) {
+        if(MRU.isChecked()) {
+            EditText xinicial, tempo , velocidade;
+
+            xinicial = findViewById(R.id.x0);
+            tempo = findViewById(R.id.tempo);
+            velocidade = findViewById(R.id.vel);
+
+            String x0Digitada = xinicial.getText().toString();
+            String tempDigitado = tempo.getText().toString();
+            String velDigitada = velocidade.getText().toString();
+
+            if(x0Digitada.equals("")  || tempDigitado.equals("") ||  velDigitada.equals("")){
+                tempo.setError("Informe o tempo");
+                xinicial.setError("Informe a distancia inicial");
+                velocidade.setError("Informe a velocidade");
+                return;
+            }
+
+            try{
+                double tempoDouble = Double.parseDouble(tempDigitado);
+                double x0Double = Double.parseDouble(x0Digitada);
+                double velDouble = Double.parseDouble(velDigitada);
+
+                double resultado =  x0Double + (velDouble*tempoDouble);
+
+                TextView result = findViewById(R.id.resultado);
+
+                result.setText("Resultado= " + resultado + " metros.");
+                result.setVisibility(View.VISIBLE);
+
+            }catch (Exception e){
+
+                Toast mensagemErro = Toast.makeText(this, "Algo errado aconteceu, evite letras", Toast.LENGTH_LONG);
+                mensagemErro.show();
+
+            }
+
+        }if(velMed.isChecked()) {
             EditText distancia, tempo;
 
             distancia = findViewById(R.id.distancia);
@@ -139,7 +214,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-
         }if(peso.isChecked()) {
             EditText mass = findViewById(R.id.massa);
             mass = findViewById(R.id.massa);
@@ -170,8 +244,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-
     }
-
 
 }
